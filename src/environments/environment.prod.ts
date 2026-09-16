b77d8e0e8d161503ba5ecacc5d666f8cdca7efc0
Elkin Omar Jimenez Garcia <jimenezelkg+GHCO@hitss.com>
2026-09-16 15:34:35 -0500

    Cambiando configuracion de IA.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  geminiApiKey: 'QUl6YVN5Q0Yxd0tFV0d6NWx1UTZ6YjF5LTNDRFVZdVFjYjZMU3J3',
+  geminiApiKey: 'QVEuQWI4Uk42SUZQWWU5ZHRvQzhkSHJYTWZHbW5udTFMWUpQdmZ0VnRibFR0UHFYYnp5M2c=',
   geminiTopicsUrl: 'https://generativelanguage.googleapis.com/v1beta/models/gemini-3.5-flash-lite:generateContent',
   klipySearchUrl: 'https://api.klipy.com/api/v1/bWmImJklrbhj0MqMLSscTXfKtmvZqypmvIhuJqKOc9D7Z2PM2lYYtFA9xXg74chi/gifs/search'
 };
